ddc22adcfb7c3987faae8119f175628f6b40c366
Johanna Smith <jojsmith@mymail.mines.edu>
2014-04-22 10:45:46 -0600

    Added TDD file (I believe we'll only have one). Just pushing now so we're all messing with the same stuff/ battery is about to die so I'm not working on it more

---
+++ src/TDD/alpineTests.java
@@ -0,0 +1,15 @@
+package TDD;
+
+import org.junit.Test;
+
+public class alpineTests {
+	//create objects of each kind
+	
+	@Test
+	public void hypotheses() {
+		
+	}
+	
+	//more things will happen, just wanted to officially add this to the repo so we'd
+	//all be working on the same file if anybody else wanted to touch it. just let me know
+}
